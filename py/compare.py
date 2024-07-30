9761bf96581e470eb9071d8b4116423b9b896cde
ksohail- <ksohail-@host404.404>
2024-07-30 13:53:26 +0100

    ...

---
+++ py/compare.py
@@ -0,0 +1,18 @@
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+
+if x != y:
+    print("x is not equal to then y")
+else:
+    print("x is equal to then y")
+
+
+
+if x < y:
+    print("x is less then y")
+elif x > y:
+    print("x is greater then y")
+# else:
+#     print("x is equal to then y")
+
+
