7ca7c63acbb3fdbc7778ef87c97a3e9ede498031
ERROR244 <khalilsohail24@gmail.com>
2025-01-02 19:33:30 +0100

    Training Piscine Python for datascience - 0 -> Done.

---
+++ py/42-Pool/Starting/ex08/Loading.py
@@ -0,0 +1,7 @@
+def ft_tqdm(lst: range) -> None: # type: ignore
+    total= len(lst)
+    for i, item in enumerate(lst, 1):
+        progress = int(254 * i / total)
+        bar = f"{'█' * progress}{' ' * (254 - progress)}"
+        print(f"\r{int(progress/2.54)}%|{bar}| {i}/{total}", end="", flush=True)
+        yield item
---
+++ py/42-Pool/Starting/ex09/test/test_module.py
@@ -0,0 +1,8 @@
+from ft_package.module import add_one, count_in_list
+
+i = 3
+j = add_one(i)
+print(j)
+
+print(count_in_list(["toto", "tata", "toto"], "toto"))
+print(count_in_list(["toto", "tata", "toto"], "tutu"))
